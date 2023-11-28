98e59a7a93ccbd93df4812ba3b5a2b301f48eb62
Sabeto051 <bedoya.santi@gmail.com>
2023-11-27 22:53:02 -0500

    first commit

---
+++ src/main/java/com/sabetomarket/SabetoMarketApplication.java
@@ -0,0 +1,13 @@
+package com.sabetomarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabetoMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SabetoMarketApplication.class, args);
+	}
+
+}
